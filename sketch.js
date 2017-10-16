ebbf7592d698b3bd16a3f55033006df5fc2d6750
Yvee1 <30909373+Yvee1@users.noreply.github.com>
2017-10-16 17:43:19 +0200

    Added sketch and hole

---
+++ sketch.js
@@ -0,0 +1,36 @@
+let h;
+
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+	h = new Hole();
+}
+
+function draw() {
+	background(50, 150, 150);
+	h.update();
+	h.show():
+}
+
+class Hole{
+	let pos;
+	let posLocked;
+	const gravity = 1;
+
+	constructor(){
+		pos = createVector(width/2, height/2);
+		posLocked = true;
+	}
+
+	update(){
+		if(!posLocked){
+			pos.x = mouseX;
+			pos.y = mouseY;
+		}
+	}
+
+	show(){
+		fill(0);
+		noStroke();
+		ellipse(pos.x, pos.y, 150, 150);
+	}
+}
